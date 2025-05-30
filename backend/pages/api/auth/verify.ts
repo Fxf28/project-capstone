96879f1a6cc7149a9943b6935ebef3779a54fddd
Fxf28 <127678603+Fxf28@users.noreply.github.com>
2025-05-30 14:55:36 +0700

    Update verify.ts

--- backend/pages/api/auth/verify.ts
+++ backend/pages/api/auth/verify.ts
@@ -5,7 +5,13 @@ import User from "@/models/User";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  const allowedOrigins = ["http://localhost:5173", "https://project-capstone-gamma.vercel.app/"];
+  const origin = req.headers.origin;
+  
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
