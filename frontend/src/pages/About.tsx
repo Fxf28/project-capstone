d6dbaadcb64a2c051b45fb666f65dd1884b5d83f
Dhio-pksd <dhioku@gmail.com>
2025-06-03 00:35:16 +0700

    update profile pada web

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -13,45 +13,40 @@ import { motion, AnimatePresence } from "framer-motion";
 export const About: React.FC = () => {
   const teamMembers = [
     {
-      name: "Orang 1",
-      role: "AI Engineer & Backend Developer",
-      photo:
-        "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=150&h=150&fit=crop&crop=face",
-      linkedin: "https://linkedin.com/in/orang1",
-      github: "https://github.com/orang1",
+      name: 'Muh Gayuh L',
+      role: 'Frontend Developer & UI/UX Designer',
+      photo: 'https://scontent.fcxp2-1.fna.fbcdn.net/v/t39.30808-6/503616214_24661467956788983_2412984694976523485_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGorPjy-wr0Mj5M1jkabb1ZYeg_5JfaMa1h6D_kl9oxrbOX9RMC4wb3uMpGaUWgrTti6cHc31SuH-lshzsEcENk&_nc_ohc=LgrAGl_oo8kQ7kNvwFjbMAq&_nc_oc=AdkN5cqqFphy4CEqNpUSsjGN6w3-bc0agbJQLSfqGNbD6p3u91VyVo8i6pSXNYB_jss&_nc_zt=23&_nc_ht=scontent.fcxp2-1.fna&_nc_gid=8w4D3LH1Fg5prpU3jX7frw&oh=00_AfLxR8b7i08XZKp3zZMbTDH6y4XUHPNG9tuZR_ZE2daaQA&oe=6843A1C8',
+      linkedin: 'https://www.linkedin.com/in/mgayuhlksn/ ',
+      github: 'https://github.com/gyhlksn'
     },
     {
-      name: "Orang 2",
-      role: "Frontend Developer & UI/UX Designer",
-      photo:
-        "https://plus.unsplash.com/premium_photo-1664536392896-cd1743f9c02c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      linkedin: "https://linkedin.com/in/orang2",
-      github: "https://github.com/orang2",
+      name: 'Dhio Pakusadewa',
+      role: 'Frontend Developer & UI/UX Designer',
+      photo: 'https://avatars.githubusercontent.com/u/185997330?s=400&u=13f06859d389a3ae60e176e9a78184531ed3edd1&v=4',
+      linkedin: 'https://www.linkedin.com/in/dhio-pakusadewa-813844287/',
+      github: 'https://github.com/Dhio-pksd'
     },
     {
-      name: "Orang 3",
-      role: "Data Scientist & ML Engineer",
-      photo:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=150&h=150&fit=crop&crop=face",
-      linkedin: "https://linkedin.com/in/orang3",
-      github: "https://github.com/orang3",
+      name: 'Fadli Ahmad Yazid',
+      role: 'Machine Learning',
+      photo: 'https://i.ibb.co/dsv0zQxp/IMG-20250119-213905-407.webp',
+      linkedin: 'https://www.linkedin.com/in/fadli-ahmad-yazid-6a2106359',
+      github: 'https://github.com/FadliAhmadYazid '
     },
     {
-      name: "Orang 4",
-      role: "Product Manager & Research",
-      photo:
-        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=150&h=150&fit=crop&crop=face",
-      linkedin: "https://linkedin.com/in/orang4",
-      github: "https://github.com/orang4",
+      name: 'Faiz Fajar',
+      role: 'Machine Learning',
+      photo: 'https://avatars.githubusercontent.com/u/127678603?v=4',
+      linkedin: 'https://www.linkedin.com/in/faiz-fajar?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app ',
+      github: 'https://github.com/Fxf28 '
     },
     {
-      name: "Orang 5",
-      role: "DevOps & System Architecture",
-      photo:
-        "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=150&h=150&fit=crop&crop=face",
-      linkedin: "https://linkedin.com/in/orang5",
-      github: "https://github.com/orang5",
-    },
+      name: 'Reza Yasa Putra',
+      role: 'Machine Learning',
+      photo: 'https://avatars.githubusercontent.com/u/144870833?v=4',
+      linkedin: 'https://www.linkedin.com/in/rezayasa-putra/',
+      github: 'https://github.com/Rezayasaputra29'
+    }
   ];
 
   const features = [
